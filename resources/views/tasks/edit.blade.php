c60a095a189b315800ce4f7724f2aa93af1bd25e
smellycat5 <prajapatichunan@gmail.com>
2023-09-22 13:53:34 +0545

    task edit function updated

--- resources/views/tasks/edit.blade.php
+++ resources/views/tasks/edit.blade.php
@@ -32,6 +32,13 @@
             <div class="row">
                 <div class="col-xs-12 col-sm-12 col-md-12">
                     <div class="form-group">
+                        <strong>Title:</strong>
+                        <input type="text" name="title" value="{{ $task->title }}"
+                            class="form-control" placeholder="Task Title">
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
                         <strong>Task Description:</strong>
                         <input type="text" name="description" value="{{ $task->description }}"
                             class="form-control" placeholder="Task Description">
@@ -39,12 +46,23 @@
                 </div>
                 <div class="col-xs-12 col-sm-12 col-md-12">
                     <div class="form-group">
+                        <strong>Priority:</strong>
+                        <select name="priority" class="form-control">
+                            <option value="urgent" {{ $task->priority === 'urgent' ? 'selected' : '' }}>Urgent</option>
+                            <option value="high" {{ $task->priority === 'high' ? 'selected' : '' }}>High</option>
+                            <option value="medium" {{ $task->priority === 'medium' ? 'selected' : '' }}>Medium</option>
+                            <option value="low" {{ $task->priority === 'low' ? 'selected' : '' }}>Low</option>
+                        </select>
+                    </div>
+                </div>
+                <div class="col-xs-12 col-sm-12 col-md-12">
+                    <div class="form-group">
                         <strong>Status:</strong>
                         <select name="status" class="form-control">
-                            <option value="0" {{ $task->status === 0 ? 'selected' : '' }}>Pending</option>
-                            <option value="1" {{ $task->status === 1 ? 'selected' : '' }}>In Review</option>
-                            <option value="2" {{ $task->status === 2 ? 'selected' : '' }}>Change Requested</option>
-                            <option value="3" {{ $task->status === 3 ? 'selected' : '' }}>Approved</option>
+                            <option value="0" {{ $task->status === "0" ? 'selected' : '' }}>Pending</option>
+                            <option value="1" {{ $task->status === "1" ? 'selected' : '' }}>In Review</option>
+                            <option value="2" {{ $task->status === "2" ? 'selected' : '' }}>Change Requested</option>
+                            <option value="3" {{ $task->status === "3" ? 'selected' : '' }}>Approved</option>
                         </select>
                     </div>
                 </div>
